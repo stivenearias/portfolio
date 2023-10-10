50767b1ffd7ca4c8ec380c5af898d1798a523037
stivenearias <bsea096@gmail.com>
2023-10-10 17:04:32 -0500

    Added object with the data of my experiences and projects to be used in the section Work

---
+++ src/components/config/work-data.js
@@ -0,0 +1,62 @@
+const workData = [
+  {
+    key: 1,
+    img: "",
+    position: "Junior Frontend Developer",
+    title: "Neuvoo to become Talent.com",
+    description: [
+      "Jobs data indexer using the DOM as a medium.",
+      "Development of internal applications based on JavaScript and PHP.",
+      "Migration of web applications.",
+    ],
+    type: "Experience",
+    date: "Sep 2019 to Dec 2020",
+    tools: ["HTML", "CSS", "JavaScript", "PHP", "Bootstrap"],
+    buttons: [
+      {
+        type: "web",
+        link: "https://www.talent.com",
+      },
+    ],
+  },
+  {
+    key: 2,
+    img: "",
+    position: "Frontend Developer",
+    title: "Talent.com",
+    description: [
+      "Development of internal web applications to provide administration of job postings to SMEs.",
+      "Contributions to web applications in the payment area.",
+      "Administration and contributions to an internal frontend framework that works under JavaScript and PHP.",
+      "SCRUM work methodology.",
+    ],
+    type: "Experience",
+    date: "Jan 2021 to Jan 2023",
+    tools: ["HTML", "CSS", "JavaScript", "PHP", "HyperUI", "JQuery"],
+    buttons: [
+      {
+        type: "web",
+        link: "https://www.talent.com",
+      },
+    ],
+  },
+  {
+    key: 3,
+    img: "",
+    position: "",
+    title: "Social Bles",
+    description:
+      "A personal blog to share thoughts, ideas, opinions and content about God.",
+    type: "Project",
+    date: "Workint on it",
+    tools: ["Mongo", "Express", "React", "Node", "SASS"],
+    buttons: [
+      {
+        type: "inProgress",
+        link: "",
+      },
+    ],
+  },
+];
+
+export default workData;
