bba3f9e31d5e812f1039c48dd8ef14d31d8732fd
stivenearias <bsea096@gmail.com>
2023-10-12 18:48:45 -0500

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { About } from "./components/About";
 // TODO: Validar si dejamos estos blurs o los quitamos
 // import { BgBlurs } from "./components/BgBlurs";
 import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
 import { ParticlesBackground } from "./components/ParticlesBackground";
@@ -19,6 +20,7 @@ function App() {
       <About />
       <Work />
       <Contact />
+      <Footer />
     </main>
   );
 }
